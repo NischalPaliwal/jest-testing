e1ffb43c575539ec082d904fee24ea0b57f5b1a1
Nischal Paliwal <156404171+NischalPaliwal@users.noreply.github.com>
2025-02-03 15:38:05 +0530

    more tests added

--- sum.test.js
+++ sum.test.js
@@ -2,8 +2,25 @@ const sum = require("./sum");
 
 test('adds two numbers a and b', () => {
     expect(sum(1, 2)).toBe(3);
-})
+});
 
 test("object assignment", () => {
-    const data = { one: 1 }
-})
+    const data = { one: 1 };
+    data["two"] = 2;
+    expect(data).toEqual({ one: 1, two: 2 });
+});
+
+test("null is falsy", () => {
+    const n = null;
+    expect(n).toBeFalsy();
+});
+
+test("zero is falsy", () => {
+    const n = 0;
+    expect(n).toBeFalsy();
+});
+
+test("one is truthy", () => {
+    const n = 1;
+    expect(n).toBeTruthy();
+});
