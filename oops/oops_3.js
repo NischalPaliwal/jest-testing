374ffb14431680988175d8cecc765300eed23113
Nischal Paliwal <156404171+NischalPaliwal@users.noreply.github.com>
2025-02-03 23:04:49 +0530

    code added

--- oops/oops_3.js
+++ oops/oops_3.js
@@ -1,10 +1,15 @@
 // inheritance
 class Parent {
+    static noOfInstances = 0;
     constructor(name, age) {
         this.name = name;
         this.age = age;
+        Parent.noOfInstances++;
     }
 
+    logParent = () => {
+        console.log(`Name of this parent is ${this.name}.`);
+    }
 }
 
 class Child extends Parent {
@@ -12,4 +17,19 @@ class Child extends Parent {
         super(name, age);
         this.father = father;
     }
+
+    logChild = () => {
+        console.log(`Name of the child of ${this.father} is ${this.name}.`);
+    }
 }
+
+const parent = new Parent("Saransh Lodha", 21);
+const child = new Child("Saksham", 18, "Saransh Lodha");
+
+parent.logParent();
+child.logChild();
+
+console.log(child instanceof Parent);
+console.log(parent instanceof Child);
+
+console.log(Parent.noOfInstances);
